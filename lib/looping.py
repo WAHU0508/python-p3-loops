079059ea31415ec23da776436fa1a5c5f98fae78
WAHU0508 <sharongikenye@gmail.com>
2024-08-25 11:35:05 +0300

    Add loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared = [square * square for square in int_list]
+    return squared
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if (i % 3 == 0):
+            if (i % 5 == 0):
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif (i % 5 == 0):
+            if (i % 3 == 0):
+                print("FizzBuzz")
+            else:
+                print("Buzz")
+        else:
+            print(f"{i}")
+        i += 1
